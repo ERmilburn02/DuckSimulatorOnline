894ebf1483d9817cc02282024850a65623afb508
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-04 19:38:33 +0100

    Update Schema
    Also removed migrations as we don't need them until production

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -5,14 +5,14 @@ import type { User } from "@prisma/client";
 const DEFAULT_USERS = [
   // Add your own user to pre-populate the database with
   {
-    name: "Tim Apple",
-    email: "tim@apple.com",
+    discordUserId: "676511901890510848",
+    displayName: "ERmilburn02 (Eliza M)",
+    username: "ermilburn02",
+    discriminator: "0",
+    avatarURL:
+      "https://cdn.discordapp.com/avatars/676511901890510848/536e2f0b4c9e7023d9b1da761446f8d3.webp",
   },
-  {
-    name: "Demi-Cat",
-    email: "admin@blackofworld.fun"
-  }
-] as Array<Partial<User>>;
+] as Array<User>;
 
 (async () => {
   debugger;
@@ -21,13 +21,16 @@ const DEFAULT_USERS = [
       DEFAULT_USERS.map((user) =>
         prisma.user.upsert({
           where: {
-            email: user.email!,
+            discordUserId: user.discordUserId!,
           },
           update: {
             ...user,
           },
           create: {
             ...user,
+            config: {
+              create: {},
+            },
           },
         })
       )

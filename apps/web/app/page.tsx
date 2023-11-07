362a6d1378fe86f54f3ebba2fa9b1b871ed19fb6
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-07 16:00:13 +0000

    Force web app main page to be dynamic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "database";
 import LeaderboardProfile from "../components/LeaderboardProfile";
 
+export const dynamic = "force-dynamic";
+
 export default async function IndexPage() {
   const users = await prisma.user.findMany();
 

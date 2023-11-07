6f26d7db7f56c82f203469731628ece409f93748
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-07 16:05:53 +0000

    Fix problems with Prisma on Vercel

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,5 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -11,5 +13,12 @@ module.exports = {
         pathname: "/avatars/**",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };

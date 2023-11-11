7535dc21036c7b88e07e4fd6fb1bc5b211925357
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-11 13:36:56 +0000

    Metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,11 @@
+import { type Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Duck Simulator",
+  description:
+    "A genre-defying phenomenon that will take you on a journey to stop a rubber duck from taking over the developer's game!",
+};
 
 export default function RootLayout({
   children,

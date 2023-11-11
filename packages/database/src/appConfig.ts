ccfb49a3a753acf098ece99f4a1ee0cb2cef0c09
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-11 12:02:59 +0000

    appConfig utility functions in database package

---
+++ packages/database/src/appConfig.ts
@@ -0,0 +1,13 @@
+import { prisma, type AppConfig } from "./client";
+
+export const getLatestAppConfig: () => Promise<AppConfig> = async () => {
+  const appConfigs = await prisma.appConfig.findMany();
+
+  if (appConfigs.length == 0) {
+    throw new Error("No AppConfig! Bot cannot start!");
+  }
+
+  appConfigs.sort((a, b) => b.version - a.version);
+
+  return appConfigs[0];
+};

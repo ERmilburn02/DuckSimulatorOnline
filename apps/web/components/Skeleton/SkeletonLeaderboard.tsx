f45ab8be684122a1b66e8eaa9301426b717343f7
Eliza Milburn <ERmilburn02@outlook.com>
2023-11-14 08:33:37 +0000

    Update Skeletons to be accurate

--- apps/web/components/Skeleton/SkeletonLeaderboard.tsx
+++ apps/web/components/Skeleton/SkeletonLeaderboard.tsx
@@ -3,7 +3,7 @@ import SkeletonLeaderboardProfile from "./SkeletonLeaderboardProfile";
 export default async function SkeletonLeaderboard() {
   return (
     <>
-      <div className="w-11/12 mx-auto flex flex-col">
+      <div className="w-11/12 mx-auto flex flex-col border-2 rounded-3xl">
         <SkeletonLeaderboardProfile />
         <SkeletonLeaderboardProfile />
         <SkeletonLeaderboardProfile />
--- apps/web/components/Skeleton/SkeletonLeaderboardProfile.tsx
+++ apps/web/components/Skeleton/SkeletonLeaderboardProfile.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function SkeletonLeaderboardProfile() {
   return (
     <>
-      <div className="grid grid-cols-level-leaderboard-mobile md:grid-cols-level-leaderboard grid-rows-2 text-center items-center justify-items-center w-full h-24 md:h-36 my-4">
+      <div className="grid grid-cols-level-leaderboard-mobile md:grid-cols-level-leaderboard grid-rows-2 text-center items-center justify-items-center w-full md:w-[98%] h-24 md:h-36 border rounded-3xl md:my-2 mx-auto">
         <div className="row-span-2 font-bold text-xl md:text-5xl m-1">#</div>
         <div className="row-span-2 overflow-hidden">
           <div className="relative w-16 h-16 md:w-32 md:h-32 m-4">

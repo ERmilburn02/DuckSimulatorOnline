43c323404af822d4349c4a1f1f131b28506666e1
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-14 14:23:51 +0000

    Bot handles Image Required channels

--- apps/discord/src/events/messageCreate.ts
+++ apps/discord/src/events/messageCreate.ts
@@ -147,6 +147,41 @@ const handleSuggestion = async (message: Message<boolean>) => {
   });
 };
 
+const handleImageRequired = async (message: Message<boolean>) => {
+  if (message.attachments.size != 1) {
+    const reply = await message.reply({
+      content: "Please upload exactly 1 image with your post!",
+    });
+    await message.delete();
+    setTimeout(async () => {
+      await reply.delete();
+    }, 5e3);
+    return;
+  }
+
+  const attachment = message.attachments.first();
+  if (attachment == undefined) {
+    throw new Error("1 attachment but undefined?!");
+  }
+
+  if (attachment.contentType == null) {
+    console.error("Attachment content type is null?!");
+    return;
+  }
+
+  if (!attachment.contentType.startsWith("image")) {
+    const reply = await message.reply({
+      content:
+        "Please ensure you're only uploading an image. If it was an image, then please contact the bot's developer.",
+    });
+    await message.delete();
+    setTimeout(async () => {
+      await reply.delete();
+    }, 5e3);
+    return;
+  }
+};
+
 const saveArrayBufferToFile: (
   arrayBuffer: ArrayBuffer,
   filePath: string
@@ -195,7 +230,15 @@ const MessageCreateEvent: DuckEvent<Events.MessageCreate> = {
 
     if (message.channelId == appConfig.SuggestionChannel) {
       await handleSuggestion(message);
+      return;
     }
+
+    appConfig.RequireImageChannels.forEach(async (ric) => {
+      if (message.channelId == ric) {
+        await handleImageRequired(message);
+        return;
+      }
+    });
   },
 };
 

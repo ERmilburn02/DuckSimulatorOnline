ffad59b2baa032bcbc7e64d2f4aa20012cb9a09b
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-14 22:42:01 +0000

    duck sim 2 page description

--- apps/web/app/duck-simulator-2/page.tsx
+++ apps/web/app/duck-simulator-2/page.tsx
@@ -1,7 +1,25 @@
 export default function DuckSimulator2Page() {
   return (
     <>
-      <>Duck Simulator 2 page</>
+      <p>
+        Duck Simulator 2 is a game about quacking a rubber duck. Seems simple
+        enough, right?
+      </p>
+
+      <p>
+        Eventually, the game begins to break and the developer is left with no
+        choice but to create several temporary games.
+      </p>
+
+      <p>
+        The game includes four genres in the campaign, a remastered version of
+        the first game, and more!
+      </p>
+
+      <p>
+        Duck Simulator 2 is currently available to download for free on XBOX and
+        Steam!
+      </p>
     </>
   );
 }

a5b111e092d293913d29cd39984ad136827f70d7
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-27 20:56:46 +0000

    secret message

--- apps/web/app/launcher/page.tsx
+++ apps/web/app/launcher/page.tsx
@@ -4,6 +4,11 @@ export default function LauncherPage() {
   return (
     <>
       <PageComingSoon />
+      <p className="absolute bottom-10 mx-10 text-transparent">
+        Seriously though, why are you here? Did you look at the source code?
+        Look, I'm not mad, just... curious. Send me a DM, let me know how you
+        found this page.
+      </p>
     </>
   );
 }

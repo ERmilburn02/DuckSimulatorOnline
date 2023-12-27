29b37657b60c01178234f1164d9eae158c84f9fb
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-27 23:01:47 +0000

    fix linting issue

--- apps/web/app/launcher/page.tsx
+++ apps/web/app/launcher/page.tsx
@@ -6,8 +6,8 @@ export default function LauncherPage() {
       <PageComingSoon />
       <p className="absolute bottom-10 mx-10 text-transparent">
         Seriously though, why are you here? Did you look at the source code?
-        Look, I'm not mad, just... curious. Send me a DM, let me know how you
-        found this page.
+        Look, I&apos;m not mad, just... curious. Send me a DM, let me know how
+        you found this page.
       </p>
     </>
   );

232ed27d584df0d36355b0a79dba57215f8c7fc6
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-28 17:15:19 +0000

    more y margin on markdown pages

--- apps/web/app/(markdown)/layout.tsx
+++ apps/web/app/(markdown)/layout.tsx
@@ -5,7 +5,7 @@ export default function MarkdownLayout({
 }) {
   return (
     <>
-      <div className="w-11/12 mx-auto my-2 max-w-full prose prose-invert text-center">
+      <div className="w-11/12 mx-auto my-4 max-w-full prose prose-invert text-center">
         {children}
       </div>
     </>

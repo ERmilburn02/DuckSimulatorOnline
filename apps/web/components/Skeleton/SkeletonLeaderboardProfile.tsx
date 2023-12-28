c0f23fd0361f3cc411f4875858ceaba95bda2fae
Eliza Milburn <ERmilburn02@outlook.com>
2023-12-28 16:46:47 +0000

    update SkeletonLeaderboardProfile to be consistent with LeaderboardProfile

--- apps/web/components/Skeleton/SkeletonLeaderboardProfile.tsx
+++ apps/web/components/Skeleton/SkeletonLeaderboardProfile.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function SkeletonLeaderboardProfile() {
   return (
     <>
-      <div className="grid grid-cols-level-leaderboard-mobile md:grid-cols-level-leaderboard grid-rows-2 text-center items-center justify-items-center w-full md:w-[98%] h-24 md:h-36 border rounded-3xl md:my-2 mx-auto">
-        <div className="row-span-2 font-bold text-xl md:text-5xl m-1">#</div>
+      <div className="grid grid-cols-level-leaderboard grid-rows-2 text-center items-center justify-items-center w-full md:w-[98%] h-24 md:h-36 border rounded-3xl md:my-2 mx-auto">
+        <div className="row-span-2 font-bold text-xl md:text-5xl m-1 overflow-hidden">
+          #
+        </div>
         <div className="row-span-2 overflow-hidden">
           <div className="relative w-16 h-16 md:w-32 md:h-32 m-4">
             <Image
@@ -20,9 +22,9 @@ export default function SkeletonLeaderboardProfile() {
         <div className="text-md md:text-3xl overflow-hidden whitespace-nowrap overflow-ellipsis w-10/12">
           Loading...
         </div>
-        <div className="text-md md:text-3xl">Loading level...</div>
-        <div className="text-md md:text-3xl">Loading XP...</div>
-        <div className="text-md md:text-3xl">Loading messages...</div>
+        <div className="text-md md:text-3xl">Loading...</div>
+        <div className="text-md md:text-3xl">Loading...</div>
+        <div className="text-md md:text-3xl">Loading...</div>
       </div>
     </>
   );

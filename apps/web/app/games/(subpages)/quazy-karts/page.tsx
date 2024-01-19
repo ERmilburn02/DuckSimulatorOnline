81c6cd3e76a8ca0f492e4c7f1edba009c802072c
Eliza Milburn <ERmilburn02@outlook.com>
2024-01-19 10:58:46 +0100

    Updated pages

--- apps/web/app/games/(subpages)/quazy-karts/page.tsx
+++ apps/web/app/games/(subpages)/quazy-karts/page.tsx
@@ -3,7 +3,13 @@ import PageComingSoon from "@/components/page-coming-soon";
 export default function QuazyKartsPage() {
   return (
     <>
-      <PageComingSoon />
+      <div className="grid place-items-center h-full w-full">
+        <div className="lg:mx-16 text-center">
+          <h1 className="font-bold lg:text-5xl text-2xl mb-2">
+            Coming Soon... <span className="italic">hopefully...</span>
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,27 @@
-import PageComingSoon from "@/components/page-coming-soon";
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+import DuckImage from "./_assets/Duck.png";
 
 export default async function IndexPage() {
   return (
     <>
-      <PageComingSoon />
+      <div className="grid place-items-center h-full w-full">
+        <div className="lg:mx-16 text-center">
+          <h1 className="font-bold lg:text-5xl text-2xl mb-2">
+            Welcome to Duck Simulator!
+          </h1>
+          <div className="mt-16 flex items-center justify-center">
+            <div className="relative w-64 h-64">
+              <Image
+                src={DuckImage}
+                alt="Image"
+                className="rounded-md object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

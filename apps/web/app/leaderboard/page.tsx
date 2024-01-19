4652369466a4f5e0547f3c01e627490e74207d61
Eliza Milburn <ERmilburn02@outlook.com>
2024-01-19 20:10:39 +0100

    Move leaderboard components
    They are now located next to the page, similar to other components

--- apps/web/app/leaderboard/page.tsx
+++ apps/web/app/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
-import Leaderboard from "../../components/Leaderboard";
-import SkeletonLeaderboard from "../../components/Skeleton/SkeletonLeaderboard";
+import Leaderboard from "./_components/Leaderboard";
+import SkeletonLeaderboard from "./_components/Skeleton/SkeletonLeaderboard";
 
 export const dynamic = "force-dynamic";
 
